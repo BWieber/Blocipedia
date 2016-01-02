f325be26aeadaee099ffffd7028252a6c69f2495
Brandon Wieber <brandon.m.wieber@gmail.com>
2016-01-02 18:07:39 -0500

    Reverted back to working code

--- db/schema.rb
+++ db/schema.rb
@@ -16,16 +16,18 @@ ActiveRecord::Schema.define(version: 20151208000213) do
   create_table "charges", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
+
+  add_index "charges", ["user_id"], name: "index_charges_on_user_id"
 
   create_table "collaborators", force: :cascade do |t|
     t.integer  "user_id"
     t.integer  "wiki_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  add_index "collaborators", ["id"], name: "index_collaborators_on_id", unique: true
   add_index "collaborators", ["user_id"], name: "index_collaborators_on_user_id"
   add_index "collaborators", ["wiki_id"], name: "index_collaborators_on_wiki_id"
 
@@ -56,10 +58,13 @@ ActiveRecord::Schema.define(version: 20151208000213) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["id"], name: "index_users_on_id", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
   create_table "users_roles", id: false, force: :cascade do |t|
@@ -78,7 +83,6 @@ ActiveRecord::Schema.define(version: 20151208000213) do
     t.datetime "updated_at",                 null: false
   end
 
-  add_index "wikis", ["id"], name: "index_wikis_on_id", unique: true
   add_index "wikis", ["user_id"], name: "index_wikis_on_user_id"
 
 end
