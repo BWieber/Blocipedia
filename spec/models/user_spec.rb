43419dad1feddda4b1b43db729bfdb8d0de3f559
Brandon Wieber <brandon.m.wieber@gmail.com>
2016-02-02 20:39:57 -0500

    added passing tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,48 +1,28 @@
 require 'rails_helper'
 
-RSpec.describe User, :type => :model do
+RSpec.describe User, type: :model do
 
   let(:user) { create(:user) }
 
-  it { should validate_presence_of(:name) }
-  it { should validate_length_of(:name).is_at_least(1) }
-
   it { should validate_presence_of(:email) }
   it { should validate_uniqueness_of(:email) }
-  it { should validate_length_of(:email).is_at_least(3) }
   it { should allow_value("user@bloccit.com").for(:email) }
-  it { should_not allow_value("userbloccit.com").for(:email) }
+  it { should_not allow_value("userbloccit").for(:email) }
 
   it { should validate_presence_of(:password) }
-  it { should have_secure_password }
-  it { should validate_length_of(:password).is_at_least(6) }
+  it { should validate_length_of(:password).is_at_least(8) }
 
-  describe "attributes" do
-    it "should respond to name" do
-      expect(user).to respond_to(:name)
+  it { should have_many(:wikis) }
+
+  context "authenticating a user" do
+    let(:user) { FactoryGirl.create(:user, password: "password123", password_confirmation: "password123") }
+
+    it "will validate a correct password" do
+      user.valid_password?("password123").should be_truthy
     end
 
-    it "should respond to email" do
-      expect(user).to respond_to(:email)
-    end
-  end
-
-  describe "invalid user" do
-
-  let(:user_with_invalid_name) { User.new(name: "", email: "user@bloccit.com") }
-  let(:user_with_invalid_email) { User.new(name: "Bloccit User", email: "") }
-  let(:user_with_invalid_email_format) { User.new(name: "Bloccit User", email: "invalid_format") }
-
-    it "should be an invalid user due to blank name" do
-      expect(user_with_invalid_name).to_not be_valid
-    end
-
-    it "should be an invalid user due to blank email" do
-      expect(user_with_invalid_email).to_not be_valid
-    end
-
-    it "should be an invalid user due to incorrectly formatted email address" do
-      expect(user_with_invalid_email_format).to_not be_valid
+    it "will not validate an incorrect password" do
+      user.valid_password?("bad-password").should be_falsey
     end
   end
 end
